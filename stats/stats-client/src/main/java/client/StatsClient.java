641333eb9e451b9879f2db8a6deeff4f16f48d7a
AlexanderChum <Senneiko@yandex.ru>
2025-10-10 00:57:55 +0300

    File transfer + add discovery and gateway features

---
+++ stats/stats-client/src/main/java/client/StatsClient.java
@@ -0,0 +1,82 @@
+package client;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import stat.dto.EndpointHitDto;
+import stat.dto.ViewStatsDto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+
+import static stat.constant.Const.DATE_TIME_FORMAT;
+
+@Service
+@SuppressWarnings("unused")
+public class StatsClient {
+
+    private final DiscoveryClient discoveryClient;
+    private final RestTemplate restTemplate;
+    private String baseUrl;
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
+
+    public StatsClient(DiscoveryClient discoveryClient, RestTemplate restTemplate) {
+        this.discoveryClient = discoveryClient;
+        this.restTemplate = restTemplate;
+    }
+
+    public ResponseEntity<EndpointHitDto> postHit(EndpointHitDto endpointHitDto) {
+        baseUrl = makeUri();
+        String url = baseUrl + "/hit";
+        return restTemplate.postForEntity(url, endpointHitDto, EndpointHitDto.class);
+    }
+
+    public List<ViewStatsDto> getStatistics(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+        String startStr = start.format(DATE_TIME_FORMATTER);
+        String endStr = end.format(DATE_TIME_FORMATTER);
+
+        baseUrl = makeUri();
+
+        StringBuilder urlBuilder = new StringBuilder(baseUrl)
+                .append("/stats?start=").append(startStr)
+                .append("&end=").append(endStr)
+                .append("&unique=").append(unique);
+
+        if (uris != null && !uris.isEmpty()) {
+            for (String uri : uris) {
+                urlBuilder.append("&uris=").append(uri);
+            }
+        }
+
+        String url = urlBuilder.toString();
+        ResponseEntity<ViewStatsDto[]> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                ViewStatsDto[].class
+        );
+        return Arrays.asList(response.getBody());
+    }
+
+    private ServiceInstance getInstance() {
+        try {
+            return discoveryClient
+                    .getInstances("stats-service")
+                    .getFirst();
+        } catch (Exception exception) {
+            throw new StatsServerUnavailable("Ошибка обнаружения адреса сервиса статистики");
+        }
+    }
+
+    private String makeUri() {
+        ServiceInstance instance = getInstance();
+        return instance.getUri().toString();
+    }
+
+}
---
+++ stats/stats-client/src/main/java/client/StatsServerUnavailable.java
@@ -0,0 +1,7 @@
+package client;
+
+public class StatsServerUnavailable extends RuntimeException {
+    public StatsServerUnavailable(String message) {
+        super(message);
+    }
+}

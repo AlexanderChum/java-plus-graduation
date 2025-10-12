ea53c41515f98ee56d1374700e2d342fddbd54b4
AlexanderChum <Senneiko@yandex.ru>
2025-10-12 19:10:15 +0300

    Pom and yaml edit

--- infra/gateway-server/src/main/java/ru/yandex/practicum/GatewayServer.java
+++ infra/gateway-server/src/main/java/ru/yandex/practicum/GatewayServer.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayService {
+public class GatewayServer {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayService.class, args);
+        SpringApplication.run(GatewayServer.class, args);
     }
 }

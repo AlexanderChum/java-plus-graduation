2129e7dabe73a053a69fd7bbd6d314304bf3d24e
AlexanderChum <Senneiko@yandex.ru>
2025-10-24 02:39:35 +0300

    Small fix

--- core/request-service/src/main/java/ru/yandex/practicum/RequestServiceApplication.java
+++ core/request-service/src/main/java/ru/yandex/practicum/RequestServiceApplication.java
@@ -3,8 +3,11 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import ru.yandex.practicum.event.PublicEventFeignClient;
 
 @EnableDiscoveryClient
+@EnableFeignClients(basePackageClasses = PublicEventFeignClient.class)
 @SpringBootApplication
 public class RequestServiceApplication {
     public static void main(String[] args) {
